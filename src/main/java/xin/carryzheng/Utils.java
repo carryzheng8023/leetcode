9e119a4d0d1008397531ece2f7631ccc3773d41d
zhengxin <carryzheng8023@gmail.com>
2019-05-14 17:08:28 +0800

    整理

--- src/main/java/xin/carryzheng/Utils.java
+++ src/main/java/xin/carryzheng/Utils.java
@@ -5,33 +5,29 @@ package xin.carryzheng;
  */
 public class Utils {
 
-    public static void traverse(int[] n){
+    public static void traverse(int[] n) {
 
         if (null == n)
             return;
 
-        for (int i=0; i<n.length; i++){
+        for (int i = 0; i < n.length; i++) {
             System.out.print(n[i] + "\t");
         }
     }
 
-    public static void preOrder(Node node){
+    public static void preOrder(Node node) {
         if (node == null) return;
         System.out.print(node + "\t");
         preOrder(node.left);
         preOrder(node.right);
     }
 
-    public static void inOrder(Node node){
+    public static void inOrder(Node node) {
         if (node == null) return;
         inOrder(node.left);
         System.out.print(node + "\t");
         inOrder(node.right);
     }
-
-    public static void f(){}
-
-
 
 
 }
