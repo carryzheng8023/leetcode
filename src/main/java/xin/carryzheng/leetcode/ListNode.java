9669f72a0888351bf4815b124c7bbacc18405f1b
zhengxin <carryzheng8023@gmail.com>
2019-05-30 15:33:23 +0800

    #203

---
+++ src/main/java/xin/carryzheng/leetcode/ListNode.java
@@ -0,0 +1,14 @@
+package xin.carryzheng.leetcode;
+
+/**
+ * @author zhengxin
+ * @date 2019-05-30 14:40:17
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/xin/carryzheng/leetcode/RemoveLinkedListElements.java
@@ -0,0 +1,53 @@
+package xin.carryzheng.leetcode;
+
+/**
+ * #203
+ * <p>
+ * 删除链表中等于给定值 val 的所有节点。
+ * <p>
+ * 示例:
+ * 输入: 1->2->6->3->4->5->6, val = 6
+ * 输出: 1->2->3->4->5
+ *
+ * @author zhengxin
+ * @date 2019-05-30 14:37:50
+ */
+public class RemoveLinkedListElements {
+
+    public ListNode removeElements(ListNode head, int val) {
+
+        while (head != null && head.val == val)
+            head = head.next;
+
+        if (head == null)
+            return null;
+
+        ListNode prev = head;
+        while (prev.next != null) {
+            if (prev.next.val == val)
+                prev.next = prev.next.next;
+            else
+                prev = prev.next;
+        }
+
+        return head;
+    }
+
+    public ListNode removeElements2(ListNode head, int val) {
+
+        ListNode dummyHead = new ListNode(-1);
+
+        dummyHead.next = head;
+
+        ListNode prev = dummyHead;
+        while (prev.next != null) {
+            if (prev.next.val == val)
+                prev.next = prev.next.next;
+            else
+                prev = prev.next;
+        }
+
+        return dummyHead.next;
+    }
+
+}
