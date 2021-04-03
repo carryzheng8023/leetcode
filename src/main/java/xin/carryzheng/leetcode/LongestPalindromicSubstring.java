ac2df42ae716913e946b440520bb9c48f3847329
zhengxin <carryzheng8023@gmail.com>
2021-04-03 20:07:06 +0800

    #5 最大回文子字符串

---
+++ src/main/java/xin/carryzheng/leetcode/LongestPalindromicSubstring.java
@@ -0,0 +1,54 @@
+package xin.carryzheng.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * #5
+ *
+ * @author zhengxin
+ * @date 2021-04-03 19:22:36
+ */
+public class LongestPalindromicSubstring {
+
+    private int start = 0;
+    private int maxLength = 1;
+
+    public static void main(String[] args) {
+
+        System.out.println(new LongestPalindromicSubstring().longestPalindrome("babad"));
+        System.out.println(new LongestPalindromicSubstring().longestPalindrome("cabbad"));
+
+    }
+
+    public String longestPalindrome(String s) {
+
+        if(s.length() < 2)
+            return s;
+
+        char[] chars = s.toCharArray();
+
+
+        for (int i=0; i<chars.length; i++){
+            expandAroundCenter(chars, i-1, i+1);
+            expandAroundCenter(chars, i, i+1);
+        }
+
+        return s.substring(start, start+maxLength);
+    }
+
+    // [a, b, a]
+    private void expandAroundCenter(char[] chars, int left, int right){
+
+        while (left >=0 && right < chars.length && chars[left] == chars[right]){
+            if(right - left + 1 > maxLength){
+                maxLength = right - left + 1;
+                start = left;
+            }
+            left--;
+            right++;
+        }
+
+    }
+}
